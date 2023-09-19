23a4c5269aeee637f574e9881b9d2212b180eecf
Laura <Laura@DESKTOP-BESSB6E>
2023-09-19 20:58:00 +0300

    Add project files.

---
+++ 3.3Uzduotis_Array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq.Expressions;
+
+namespace _3._3Uzduotis_Array
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] ints = { 4, 7, 9 };
+
+            int[] result = AddElement(2, ints);
+
+            for (int k = 0; k < result.Length; k++)
+            {
+                Console.WriteLine(result[k]);
+            }
+        }
+        public static int[] AddElement(int newElement, int[] ints)
+        {
+            int[] newArray = new int[ints.Length + 1];
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                newArray[i] = ints[i];
+
+            }
+            newArray[newArray.Length - 1] = newElement;
+
+            return newArray;
+        }
+    }
+}
+
+
+
